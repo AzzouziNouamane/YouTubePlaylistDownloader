45f3408a27127146ed9215bf468de135bc3eaaa7
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2019-07-26 22:55:29 +0100

    1st Part Done : Script now checks if a new video is added to the playlist and returns the ID of the new video

--- playlist_items.py
+++ playlist_items.py
@@ -15,21 +15,7 @@ import json
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "client_secret_1051928112662-sp9fsvirijfvkblh2fpvkupe1kpd8u1d.apps.googleusercontent.com.json"
-
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+def main(youtube):
 
     request = youtube.playlistItems().list(
         part="contentDetails",
@@ -43,7 +29,7 @@ def main():
 
     videoId = request.execute()['items'][itemsCount-1]['contentDetails']['videoId']
     print(videoId)
-
+    return videoId
 
 #lastVideoId = 
 
